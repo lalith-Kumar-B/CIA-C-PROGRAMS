2854b6c42e3f1000df1cc2da7b4f32936cf1be2d
lalith-Kumar-B <143936185+lalith-Kumar-B@users.noreply.github.com>
2023-10-03 13:59:19 +0530

    Update so_odd_no.c
    update regarding n value

--- so_odd_no.c
+++ so_odd_no.c
@@ -1,12 +1,12 @@
 //To find the sum of the N terms of the series S= 1+ 3 + 5 + . . . N terms
 #include<stdio.h>
 int main(){
-	int sum=0,n,i;
+	int sum=0,n,i,term;
 	printf("Program To find the sum of the N terms of the series S= 1+ 3 + 5 + . . . N terms\nEnter the value of N:");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++){
-		if(i%2!=0){
-			sum+=i;
+		 term=(2*i)-1;
+			sum+=term;
 		}
 	}
 	printf("The sum of the series is %d",sum);
