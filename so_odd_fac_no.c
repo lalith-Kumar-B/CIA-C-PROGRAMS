9bbcbb1ab11c85220426543f299ba19ef8783d44
lalith-Kumar-B <143936185+lalith-Kumar-B@users.noreply.github.com>
2023-10-03 13:38:50 +0530

    Update so_odd_fac_no.c
    updates regarding 'n' value has been made

--- so_odd_fac_no.c
+++ so_odd_fac_no.c
@@ -1,17 +1,16 @@
-// program To find the sum of N terms of the series S =1! + 3! + 5! + …N terms.
+// program To find the sum of N terms of the series S =1! + 3! + 5! + â€¦N terms.
 #include<stdio.h>
 int main(){
-	int sum=0,n,i,j,m;
+	int sum=0,n,i,j,m,term;
 	printf("Program To find the sum of the N terms of the series  S =1! + 3! + 5! +... N terms\nEnter the value of N:");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++){
-		if(i%2!=0){
-			m=i;
+		term=(2*i)-1;
+			m=term;
 			for(j=m-1;j>1;j--){
 				m=m*j;
 			}
 			sum+=m;
-		}
 	}
 	printf("The sum of the series is %d",sum);
 	return(0);
