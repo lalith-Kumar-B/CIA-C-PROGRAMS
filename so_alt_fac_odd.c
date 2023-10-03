182655b3b053d35e7f2784e151f4557d22d0fac6
lalith-Kumar-B <143936185+lalith-Kumar-B@users.noreply.github.com>
2023-10-03 13:28:35 +0530

    Update so_alt_fac_odd.c
    changes regarding 'n' value has been made .

--- so_alt_fac_odd.c
+++ so_alt_fac_odd.c
@@ -1,11 +1,12 @@
-// c program To find the sum of N terms of the series S =1! - 3! + 5! - …N terms.
+// c program To find the sum of N terms of the series S =1! - 3! + 5! - â€¦N terms.
 #include<stdio.h>
 int main(){
 	int sum=0,n,i,j,m,sign=1;
 	printf("Program To find the sum of the N terms of the series  S =1! - 3! + 5! -... N terms\nEnter the value of N:");
 	scanf("%d",&n);
-	for(i=1;i<=n;i+=2){
-			m=i;
+	for(i=1;i<=n;i++){
+    term=(2*i)-1;
+			m=term;
 			for(j=m-1;j>1;j--){
 				m=m*j;
 			}
